2a66ab04571cc8bdc95ff91076e22a1cc13b4297
Milan Malfait <m.malfait@ucl.ac.uk>
2023-08-24 12:40:27 +0100

    Remove S3 policy
    Not needed right now (I think)

--- modules/appstream/security.tf
+++ modules/appstream/security.tf
@@ -19,7 +19,4 @@ data "aws_iam_policy_document" "instance_assume_role_policy" {
 resource "aws_iam_role" "as2_role" {
   name               = "ARC-AS2-S3-Role"
   assume_role_policy = data.aws_iam_policy_document.instance_assume_role_policy.json
-  managed_policy_arns = [
-    aws_iam_policy.s3_policy.arn
-  ]
 }
