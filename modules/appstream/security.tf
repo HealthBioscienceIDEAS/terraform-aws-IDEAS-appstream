b3ab448a110ce872276a870d4d933c5d87636043
Milan Malfait <m.malfait@ucl.ac.uk>
2023-08-24 15:29:49 +0100

    Remove the IAM role settings
    Not required.

--- modules/appstream/security.tf
+++ modules/appstream/security.tf
@@ -3,20 +3,3 @@ resource "aws_security_group" "appstream" {
   description = "Allow S3 egress from AppStream"
   vpc_id      = var.vpc_id
 }
-
-data "aws_iam_policy_document" "instance_assume_role_policy" {
-  statement {
-    effect  = "Allow"
-    actions = ["sts:AssumeRole"]
-
-    principals {
-      type        = "Service"
-      identifiers = ["appstream.amazonaws.com"]
-    }
-  }
-}
-
-resource "aws_iam_role" "as2_role" {
-  name               = "ARC-AS2-S3-Role"
-  assume_role_policy = data.aws_iam_policy_document.instance_assume_role_policy.json
-}
