9874669a3468afa8bbb3c782e04f74557ef6c757
Milan Malfait <m.malfait@ucl.ac.uk>
2023-08-24 12:33:30 +0100

    Update root variables

--- variables.tf
+++ variables.tf
@@ -17,65 +17,55 @@ variable "vpc_cidr_block" {
 }
 
 # AppStream vars
-variable "desired_instance_num" {
+variable "as2_desired_instance_num" {
   type        = number
   description = "Desired number of AS2 instances"
   default     = 1
 }
 
-variable "fleet_name" {
+variable "as2_fleet_name" {
   type        = string
   description = "Fleet name"
   default     = "IDEAS-fleet"
 }
 
-variable "fleet_description" {
+variable "as2_fleet_description" {
   type        = string
   description = "Fleet description"
-  default     = "IDEAS fleet"
+  default     = "IDEAS process fleet"
 }
 
-variable "fleet_display_name" {
+variable "as2_fleet_display_name" {
   type        = string
   description = "Fleet diplay name"
-  default     = "IDEAS fleet"
+  default     = "IDEAS process fleet"
 }
 
-variable "fleet_subnet_ids" {
-  type        = list(string)
-  description = "Subnet IDs for fleet"
-}
-
-variable "image_name" {
+variable "as2_image_name" {
   type        = string
   description = "AS2 image to deploy"
 }
 
-variable "instance_type" {
+variable "as2_instance_type" {
   type        = string
   description = "AS2 instance type"
   default     = "stream.standard.medium"
 }
 
-variable "stack_name" {
+variable "as2_stack_name" {
   type        = string
   description = "Stack name"
   default     = "IDEAS-stack"
 }
 
-variable "stack_description" {
+variable "as2_stack_description" {
   type        = string
   description = "Stack description"
-  default     = "IDEAS stack"
+  default     = "IDEAS process stack"
 }
 
-variable "stack_display_name" {
+variable "as2_stack_display_name" {
   type        = string
   description = "Stack diplay name"
-  default     = "IDEAS stack"
-}
-
-variable "vpc_id" {
-  type        = string
-  description = "VPC ID"
+  default     = "IDEAS process stack"
 }
