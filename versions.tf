281a6b853a838325033e16681b8e1d1f81821183
Milan Malfait <m.malfait@ucl.ac.uk>
2023-08-24 10:56:59 +0100

    Formatting

--- versions.tf
+++ versions.tf
@@ -2,8 +2,8 @@
 terraform {
   required_providers {
     aws = {
-     source  = "hashicorp/aws"
-     version = "~> 4.45.0"
+      source  = "hashicorp/aws"
+      version = "~> 4.45.0"
     }
   }
 
