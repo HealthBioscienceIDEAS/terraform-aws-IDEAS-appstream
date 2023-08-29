d53e2c7466232b0f1001419ce8947b6a6db118b1
Milan Malfait <m.malfait@ucl.ac.uk>
2023-08-29 11:54:14 +0100

    Pass VPC as a variable

--- main.tf
+++ main.tf
@@ -15,8 +15,8 @@ module "appstream" {
   fleet_name           = var.as2_fleet_name
   fleet_description    = var.as2_fleet_description
   fleet_display_name   = var.as2_fleet_display_name
-  vpc_id               = module.vpc.vpc_id
-  fleet_subnet_ids     = module.vpc.private_subnets
+  vpc_id               = var.vpc_id
+  fleet_subnet_ids     = var.private_subnets
   image_name           = var.as2_image_name
   instance_type        = var.as2_instance_type
   stack_name           = var.as2_stack_name
